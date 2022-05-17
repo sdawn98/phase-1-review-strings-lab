86bb05f0537724de0ab83671c31dd37a1a41251d
Steven <stevendawn98@gmail.com>
2022-05-17 17:47:22 -0400

    reworked string lab

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
-const currentUser = '${name}'
+const currentUser = 'name'
 
-const welcomeMessage = 'Welcome to Flatbook, ${name}!'
+const welcomeMessage = `Welcome to Flatbook, ${currentUser}!`;
 
 const excitedWelcomeMessage = welcomeMessage.toUpperCase();
 
-const shortGreeting = 'Welcome, ${currentUser[0]} + ${currentUser.slice(1)}!'
+const shortGreeting = 'Welcome, ${currentUser[0]} + ${currentUser.slice(1)}!';
